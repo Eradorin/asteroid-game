7c13c48baa30ae06cab3edeb24a4e16f237bfa31
Eradorin <e_van11@hotmail.com>
2025-03-14 13:29:51 -0600

    Add update to main.py and constants.py file

---
+++ main.py
@@ -0,0 +1,14 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import constants.py
+
+def main():
+    
+    print("Starting Asteroids!")
+    print(SCREEN_WIDTH,
+          SCREEN_HEIGHT)
+
+if __name__ == "__main__":
+    main()
